1e172eebeedb642dbd7495d323acd227c13ce2cd
mouriya1 <mouriya.signatures1@gmail.com>
2025-02-20 13:15:30 +0530

    p tag text removed

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
                     </div>
                     <div class="introwrap">
                         <p id="scrollText">Reinvigorate your designs and brand with a fresh boost to think bigger, inspiring people to imagine themselves in a larger sphere.</p>
-                    <p>Lorem ipsumgjhgjhg sit amet consectetur adipisicing elit. Vitae, soluta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero fugit doloribus cupiditate ab, reiciendis dignissimos quod aut vel est magnam velit deleniti nostrum molestiae inventore, harum provident eaque veniam molestias?</p>
+                    <p>Lorem  Vitae, soluta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero fugit doloribus cupiditate ab, reiciendis dignissimos quod aut vel est magnam velit deleniti nostrum molestiae inventore, harum provident eaque veniam molestias?</p>
                     </div>
                 </div>
             </div>
